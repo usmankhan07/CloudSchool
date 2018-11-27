1304e34fdef02605ea50b6e6c6cb4d33a765f2cd
Usman Khan <muhammadusmankhan307@gmail.com>
2018-11-27 07:39:10 +0500

    with cloudschoolbackend having database.

--- backend/models/todoList.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const TodoList = new Schema({
-  userId: {type: Schema.Types.ObjectId, ref:'LoginAccount'},
-  items : {type:String},
-
-});
-module.exports = mongoose.model("TodoList", TodoList);
